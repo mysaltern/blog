99548521e2f7001231ddccd767c077106c7d3e05
mysaltern <mysaltern@gmail.com>
2020-03-07 22:28:11 +0330

    edit main

--- milad/protected/config/main.php
+++ milad/protected/config/main.php
@@ -21,21 +21,21 @@ return array(
             // enable cookie-based authentication
             'allowAutoLogin' => true,
         ),
-        'db' => array(
-            'connectionString' => 'sqlite:protected/data/blog.db',
-            'tablePrefix' => 'tbl_',
-        ),
-        // uncomment the following to use a MySQL database
-        /*
-          'db'=>array(
-          'connectionString' => 'mysql:host=localhost;dbname=blog',
-          'emulatePrepare' => true,
-          'username' => 'root',
-          'password' => '',
-          'charset' => 'utf8',
+        /*  'db' => array(
+          'connectionString' => 'sqlite:protected/data/blog.db',
           'tablePrefix' => 'tbl_',
           ),
-         */
+         * */
+
+        // uncomment the following to use a MySQL database
+        'db' => array(
+            'connectionString' => 'mysql:host=localhost;dbname=blog',
+            'emulatePrepare' => true,
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8',
+            'tablePrefix' => 'tbl_',
+        ),
         'errorHandler' => array(
             // use 'site/error' action to display errors
             'errorAction' => 'site/error',
